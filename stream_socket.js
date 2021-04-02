38912aeb0cea2fc5ae1b203b5871ba1e6989d0c4
codabool <codabool@pm.me>
2021-04-02 16:28:31 -0400

    better conditionals

--- stream_socket.js
+++ stream_socket.js
@@ -7,37 +7,102 @@ const token = process.env.SOCKET_API_TOKEN
 const password = process.env.MOM_PASSWORD
 const cwd = '/home/codabool/scripts/alexa'
 
-console.log(token)
 const streamlabs = io(`https://sockets.streamlabs.com?token=${token}`, {transports: ['websocket']})
 
 streamlabs.on('connect', () => console.log('connected!'))
 streamlabs.on("connect_error", err => console.log(err))
 
 streamlabs.on('event', e => {
-  console.log(e.type)
   let routine = ''
+  let message = ''
+  let newEvent = ''
   if (e.type === 'subscription') {
+    message = e.message[0].name + ' joined UwU gang'
+    routine = 'donation'
   } else if (e.type === 'follow') {
-    routine = 'Switch'
+    message = 'Welcome ' + e.message[0].name
+    routine = 'basic'
   } else if (e.type === 'resub') {
-    if (e.message[0].months > 6) {
-      // routine = 'Switch'
+    routine = 'basic'
+    message = e.message[0].name + ' resubbed'
+    if (e.message[0].streak_months > 6) {
+      message = e.message[0].name + ' is on a ' + e.message[0].streak_months + ' month streak, pog'
+      if (e.message[0].streak_months > 12) {
+        message = e.message[0].name + ' is on a ' + e.message[0].streak_months + ' month streak, pog u pog'
+        if (e.message[0].streak_months > 24) {
+          message = 'simp alert' + e.message[0].name + ' is on a ' + e.message[0].streak_months + ' month streak'
+        }
+      }
     }
-    
   } else if (e.type === 'donation') {
-    if (e.message[0].amount > 5) { // amount in dollars
-      routine = 'Bye'
+    if (e.message[0].amount > 20) { // amount in dollars
+      routine = 'special'
+      message = 'pog ' + e.message[0].name + ' donated ' + e.message[0].amount + ' dollars'
+    } else if (e.message[0].amount > 5) { // amount in dollars
+      routine = 'donation'
+      message = e.message[0].name + ' donated ' + e.message[0].amount + ' dollars'
+    } else {
+      routine = 'basic'
     }
   } else if (e.type === 'host') {
-    
+    routine = 'basic'
+    if (e.message[0].viewers > 20) {
+      routine = 'donation'
+      message = e.message[0].name + ' just hosted'
+    }
+    if (e.message[0].viewers > 100) {
+      routine = 'special'
+      message = e.message[0].name + ' just hosted with ' + e.message[0].viewers + ' viewers'
+    }
+    if (e.message[0].name === 'linusfrog') {
+      message = 'linus and ' + e.message[0].viewers + ' froggies join the chat'
+    }
+    if (e.message[0].name === 'trashabag') {
+      message = 'trasha and ' + e.message[0].viewers + ' pieces of trash join the chat'
+    }
   } else if (e.type === 'bits') {
-    if (Number(e.message[0].amount) > 500) { // $5
-      routine = 'Sony'
+    if (Number(e.message[0].amount) > 2000) { // $20
+      routine = 'special'
+      message = 'pog ' + e.message[0].name + ' gave ' + e.message[0].amount + ' bits'
+    } else if (Number(e.message[0].amount) > 500) { // $5
+      routine = 'donation'
+      message = e.message[0].name + ' gave ' + e.message[0].amount + ' bits'
+    } else {
+      routine = 'basic'
     }
   } else if (e.type === 'raid') {
-    
+    routine = 'basic'
+    if (e.message[0].raiders > 20) {
+      routine = 'donation'
+      message = e.message[0].name + ' just raided'
+    }
+    if (e.message[0].raiders > 100) {
+      routine = 'special'
+      message = e.message[0].name + ' just raided with ' + e.message[0].viewers + ' viewers'
+    }
+    if (e.message[0].name === 'linusfrog') {
+      message = 'linus and ' + e.message[0].raiders + ' froggies joined the chat'
+    }
+    if (e.message[0].name === 'codabool') {
+      message = 'coda and ' + e.message[0].raiders + ' nerds joined the chat'
+    }
+    if (e.message[0].name === 'trashabag') {
+      message = 'trasha and ' + e.message[0].raiders + ' pieces of trash joined the chat'
+    }
   } else { // some other event
-    // routine = something
+    console.log('strange event', e)
+    newEvent = e.type
+  }
+
+  console.log(e.type, '|', routine, 'routine')
+  if (e.message[0].raiders) {
+    console.log(e.message[0].raiders, 'raiders')
+  }
+  if (e.message[0].viewers) {
+    console.log(e.message[0].viewers, 'viewers')
+  }
+  if (e.message[0].amount) {
+    console.log(e.message[0].amount, 'amount')
   }
 
   if (routine) {
@@ -48,19 +113,20 @@ streamlabs.on('event', e => {
       password,
       tryKeyboard: true,
     })
-      .then(res => {
-        // console.log(res)
+      .then(() => {
         ssh.execCommand('echo $(( `expr $(date +%s) - $(head -n 1 lastRan)` > 12 ))', { cwd })
-          .then(res => {
+          .then(response => {
             // will stdout a 0 = too soon
             // will stdout a 1 = ready
-            if (res.stdout === '1') {
-              ssh.execCommand(`./local.sh -d 'Echo Bed' -e automation:${routine}`, { cwd })
+            if (response.stdout === '1') {
+              if (newEvent) { // log any unhandled events
+                ssh.execCommand(`echo ${JSON.stringify(newEvent)} >> log`, { cwd })
+                    .then(res => console.log('creating log', res.stdout))
+                    .catch(err => console.log('ERROR: in creating log |', err))
+              }
+              ssh.execCommand(`./local.sh -d 'Echo Office' -e automation:${routine}`, { cwd })
                 .then(res => {
                   ssh.execCommand('touch resetSoon', { cwd })
-                    .then(res => {
-                      console.log('creating resetSoon', res.stdout)
-                    })
                     .catch(err => {
                       console.log('ERROR: in creating resetSoon |', err)
                     })
@@ -72,9 +138,44 @@ streamlabs.on('event', e => {
               console.log('DEBUG: too soon')
             }
           })
+          setTimeout(() => {
+            if (message) {
+              ssh.execCommand(`./local.sh -d 'Echo Office' -e speak:'${message}'`, { cwd })
+                .then(res => console.log('said =', message))
+                .catch(err => console.log('new', err))
+            }
+          }, 5000)
       })
       .catch(err => {
         console.log(err)
       })
   }
 })
+
+
+// ====== EVENTS ======
+// follow 
+//   e.message[0].name
+// subscription
+//   e.message[0].name
+//   e.message[0].months
+//   e.message[0].message
+// resub
+//   e.message[0].name
+//   e.message[0].months
+//   e.message[0].streak_months
+//   e.message[0].message
+// donation
+//   e.message[0].name
+//   e.message[0].amount
+//   e.message[0].message
+// hosting
+//   e.message[0].name
+//   e.message[0].viewers
+// bits
+//   e.message[0].name
+//   e.message[0].amount (string)
+//   e.message[0].message
+// raid
+//   e.message[0].name
+//   e.message[0].raiders
